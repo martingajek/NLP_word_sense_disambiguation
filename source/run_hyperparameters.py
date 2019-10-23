53c5d4bec742c87c34d9cffaa8d88da087b3a7d1
tintamarre1 <tinmarleroutard@yahoo.fr>
2019-10-23 01:11:10 +0000

    Changed arguments

--- source/run_hyperparameters.py
+++ source/run_hyperparameters.py
@@ -64,6 +64,8 @@ if __name__ == '__main__':
                         help="Input data path")
     parser.add_argument("--test_data_path", type=str, default="",
                         help="Input test data path")
+    parser.add_argument("--enable_checkpoints", type=bool, default=True,
+                        help="Save chackpoints to path")                    
     parser.add_argument('--val_check_interval', type=float, default=1.0,
                         help='Fraction of epoch at which to check validation set')
     parser.add_argument("--default_save_path", type=str, default="../data/",
@@ -73,8 +75,6 @@ if __name__ == '__main__':
     parser.add_argument("--model", type=str, default='bert') # can be xlnet as well
     parser.add_argument("--model_type", type=str, default='bert-base-uncased',choices=['bert-base-uncased','bert-base-cased'],
                         help="bert model: default is bert-base-uncased")
-    parser.add_argument("--preprocess_inputs", type=str2bool, default=True,
-                        help="Preprocess input data (Tokenize and generate input embeddings)")
     parser.add_argument("--input_len", type=int, default=128,
                         help="Sentence max length/padding size",choices=[128,256])
     parser.add_argument('--class_weights', nargs='+', type=float,default=(1.0,1.0), 
@@ -83,10 +83,10 @@ if __name__ == '__main__':
                         help="Number trials for hyperparameter optimization")
     parser.add_argument('--train_samples', type=int, default=None,
                         help="Number trials for hyperparameter optimization")                    
-    parser.add_argument('--scheduler', type=bool,default=False, 
+    parser.opt_list('--scheduler', type=bool,default=False,options=[True,False],tunable=False,
                         help="Enable cosine scheduler for optimizer")
     parser.opt_list('--batch_size', type=int, default=16, options=[8,16,32,64],
-                        help='input batch size for training (default: 64)',tunable=False)    
+                        help='input batch size for training (default: 64)',tunable=True)    
     parser.opt_range('--lr', type=float, default=2e-5,low=2E-8, high=2E-3,nb_samples=10, log_base=10,
                         help='learning rate (default: 0.01)',tunable=True)
     parser.opt_range('--weight_decay', type=float, default=0.01,low=1E-5,
